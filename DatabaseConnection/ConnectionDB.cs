337df86790ed0e3025aaa601a5b7994681cdf8b5
Михаил Миронов <mikhail.mironov.entertainment@gmail.com>
2022-06-20 15:57:33 +0300

    Первый коммит. Добавлен вывод результата запроса к БД без оформления.

---
+++ DatabaseConnection/ConnectionDB.cs
@@ -0,0 +1,14 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace test_project_products_category_csharp.DatabaseConnection
+{
+    public class ConnectionDB
+    {
+        static readonly string server = "127.0.0.1";
+        static readonly string port = "3306";
+        static readonly string database = "test_project_product_category_csharp";
+        static readonly string username = "root";
+        static readonly string password = "";
+        public static readonly MySqlConnection Connection = new MySqlConnection($"server={server}; port={port}; database={database}; username={username}; password={password}");
+    }
+}
---
+++ Views/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace test_project_products_category_csharp1.Views
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
